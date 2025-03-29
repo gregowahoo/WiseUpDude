bb976bbe1d9e7170e62ba8418713451a06da5e6b
gregowahoo <greg.ohlsen@gmail.com>
2025-03-29 15:06:00 -0500

    Celebration with applause and confetti :-)

---
+++ wwwroot/js/celebration.js
@@ -0,0 +1,15 @@
+﻿window.launchConfetti = () => {
+    confetti({
+        particleCount: 150,
+        spread: 100,
+        origin: { y: 0.6 }
+    });
+};
+
+window.playVictorySound = () => {
+    const sound = document.getElementById("victory-sound");
+    if (sound) {
+        sound.currentTime = 0;
+        sound.play();
+    }
+};

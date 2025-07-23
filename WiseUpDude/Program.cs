3785fa24d64b389b92815873f5e17e4aab5ba228
gregowahoo <greg.ohlsen@gmail.com>
2025-07-23 14:53:23 -0500

    Update database connection to use SQL Server
    Modified `Program.cs` to configure the database context for SQL Server instead of SQLite. Updated the `appsettings.json` file to reflect the new SQL Server connection string for `DefaultConnection`.

--- WiseUpDude/Program.cs
+++ WiseUpDude/Program.cs
@@ -108,7 +108,7 @@ builder.Services.ConfigureApplicationCookie(options =>
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString)); 
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
